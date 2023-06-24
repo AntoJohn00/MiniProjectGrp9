20037c24e79b8e28abfe898a75bd0801fc938ec4
Anantha krishnan <71790345+Ananthu14661@users.noreply.github.com>
2023-06-24 14:27:21 +0530

    Add files via upload

---
+++ contact/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/remixicon@3.2.0/fonts/remixicon.css"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="styles.css" />
+    <title>Contact Page</title>
+  </head>
+  <body>
+    <div class="section__container">
+      <div class="navbar">
+        <div class="navbar-logo">
+          <img src="/Main Home page/images/reactlogo2.png" alt="Logo">
+          <span>ATOM</span>
+        </div>
+        <ul>
+            <li><a href="/Main Home page/index.php">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            
+          </ul>
+        <div class="navbar-signin">
+          <a href="\login system\login_form.php">Sign In</a>
+        </div>
+    </div>
+      <div class="header">
+        <br>
+        <h2>CONTACT</h2>
+        <br>
+        <h1>Our branchs includes</h1>
+      </div>
+      <div class="testimonials__grid">
+        
+        <div class="card">
+          <span>&#128205;</span>
+          <p>
+            near police station
+          </p>
+          <hr />
+         
+          <p class="name">Clay Washington</p>
+        </div>
+
+        <div class="card">
+          <span>&#128205;</span>
+          <p>
+            near police station
+          </p>
+          <hr />
+         
+          <p class="name">Clay Washington</p>
+        </div>
+       
+        
+      </div>
+    
+    </div>
+  </body>
+</html>
